5147e1f4625a71709a837a131b12f0b79496d405
Sergey Patrushev <PatrushevSergeyVal@yandex.ru>
2023-04-11 21:34:54 +0300

    admin panel

--- up_trader_project/tree_menu/admin.py
+++ up_trader_project/tree_menu/admin.py
@@ -1,1 +1,19 @@
 from django.contrib import admin
+
+from tree_menu.models import Item, Menu
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    """
+    Defines the interface of the "Menu" model in the admin panel.
+    """
+    list_display = ('name',)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    """
+    Defines the interface of the "Item" model in the admin panel.
+    """
+    list_display = ('name', 'parent')
